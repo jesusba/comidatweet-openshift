78579f255e40ccc6ddd520f54dedb7d579fea7db
jesusba <jesusba60@gmail.com>
2014-05-02 16:13:00 +0200

    a

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,17 +1,53 @@
-from bottle import route, default_app
+from bottle import get, post, template, run, request, route, default_app
+import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
 
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
- 
-@route('/')
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+CONSUMER_KEY = "dfy0QjVcTRWQ4ATV4tbOkJCw0"
+CONSUMER_SECRET = "lYG1Ywk25TVt8jDzJ52AkNTXZouvbUiXiY8riFOvitQQvhCbWT"
+TOKENS = {}
+
+#Definiendo funciones para autenticacion en Twitter
+
+#Acceso a la API mediante parametros del cliente
+def get_request_token():
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+    )
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    TOKENS["request_token"] = credentials.get('oauth_token')[0]
+    TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
+
+#Token de solicitud
+def get_access_token(TOKENS):
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["request_token"],
+                   resource_owner_secret=TOKENS["request_token_secret"],
+                   verifier=TOKENS["verifier"],
+    )
+
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    TOKENS["access_token"] = credentials.get('oauth_token')[0]
+    TOKENS["access_token_secret"] = credentials.get('oauth_token_secret')[0]
+
+@get('/')
 def index():
-    return '<strong>Hello World!</strong>'
-
-# This must be added in order to do correct path lookups for the views
+    get_request_token()
+    authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
+    return template('index.tpl', authorize_url=authorize_url)
+    
 import os
 from bottle import TEMPLATE_PATH
-TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
-    'runtime/repo/wsgi/views/')) 
 
-application=default_app()
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
+    'app-root/runtime/repo/wsgi/views/')) 
+
+application=bottle.default_app()
+

87782d78c95a4f474521192479c69a496294b4e8
jesusba <jesusba60@gmail.com>
2014-05-02 16:22:21 +0200

    Añadida función para twittear

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -42,7 +42,29 @@ def index():
     get_request_token()
     authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
     return template('index.tpl', authorize_url=authorize_url)
+
+#Twiteando
+@bottle.post('/twittear')
+def tweet_submit():
+    def get_verifier():
+        TOKENS["verifier"] = bottle.request.query.oauth_verifier
+        get_access_token(TOKENS)
+    texto = "Tweet enviado desde openshift"
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["access_token"],
+                   resource_owner_secret=TOKENS["access_token_secret"])
     
+    url = 'https://api.twitter.com/1.1/statuses/update.json'
+
+    r = requests.post(url=url,
+                      data={"status":texto},
+                      auth=oauth)
+    if r.status_code == 200:
+        return "<p>Tweet enviado correctamente.</p><form><input type='button' value='VOLVER ATRÁS' name='Back2' onclick='history.back()' /></form>"
+    else:
+        return "<p>Problema al enviar el tweet.</p><form><input type='button' value='VOLVER ATRÁS' name='Back2' onclick='history.back()' /></form>"
+
 import os
 from bottle import TEMPLATE_PATH
 

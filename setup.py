5de4df37e48bca9741d1ab91ccf84b1a7bd49073
jesusba <jesusba60@gmail.com>
2014-05-02 16:02:00 +0200

    a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='comidatweet',
       version='1.0',
       description='OpenShift App',
-      author='Jesús Bueno',
+      author='Jesus Bueno',
       author_email='example@123.com',
       url='comidatweet-jesusba.rhcloud.com',
       install_requires=['bottle'

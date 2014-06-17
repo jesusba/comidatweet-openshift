95e4db60ff8022e8cf897de155b757128bf4f16f
jesusba <jesusba60@gmail.com>
2014-06-17 10:57:58 +0200

    Probando codigo

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -23,12 +23,7 @@ def get_request_token():
     TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
 
 def get_access_token(TOKENS):
-    oauth = OAuth1(CONSUMER_KEY,
-                   client_secret=CONSUMER_SECRET,
-                   resource_owner_key=TOKENS["request_token"],
-                   resource_owner_secret=TOKENS["request_token_secret"],
-                   verifier=TOKENS["verifier"],
-    )
+    oauth = OAuth1(CONSUMER_KEY,client_secret=CONSUMER_SECRET,resource_owner_key=TOKENS["request_token"],resource_owner_secret=TOKENS["request_token_secret"],verifier=TOKENS["verifier"],)
 
     r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
     credentials = parse_qs(r.content)
